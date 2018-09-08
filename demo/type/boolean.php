3103a4021eb33e57cdeeb9f5d27c96eaa04b700f
cheenbiao <nichtse@gmail.com>
2018-09-08 10:48:25 +0800

    init

---
+++ demo/type/boolean.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/8
+ * Time: 10:34
+ */
+var_dump((bool) "");
+var_dump((bool) 1);
+var_dump((bool) -2);
+var_dump((bool) "foo");
+var_dump((bool) 2.3e5);
+var_dump((bool) array(12));
+var_dump((bool) array());
+var_dump((bool) "false");
+
+$name = '黑子';
+echo $name;
+
+echo '<br/>';
+
+echo $name;
